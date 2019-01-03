66bbcff425435fc4f9112b4d8e7b5d0a225d4e89
Raymond von Mizener <vmizener@gmail.com>
2019-01-03 14:48:19 -0800

    moved assets to its own directory

--- lessons/02/main.js
+++ lessons/02/main.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./vmSocks-green.png",
+        image: "../assets/vmSocks-green.png",
         altText: "A pair of socks",
         link: "https://not.a.real.url",
         linkText: "not a real link"
