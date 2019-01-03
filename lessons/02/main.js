d3174af5b08f0dd577643b02abdc28fa6f996b7e
Raymond von Mizener <vmizener@gmail.com>
2019-01-03 14:23:45 -0800

    lesson 2

---
+++ lessons/02/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./vmSocks-green.png",
+        altText: "A pair of socks",
+        link: "https://not.a.real.url",
+        linkText: "not a real link"
+    }
+})
+
